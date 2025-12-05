99186bd043bce3d8d94826bca1adc935b9094159
s.vanichkin <github@macflash.ru>
2025-12-05 21:37:30 +0300

    Logger fixed

---
+++ device/camlog_hook.go
@@ -0,0 +1,32 @@
+package device
+
+import (
+	"log"
+	"strings"
+
+	"say/logs"
+	_ "unsafe"
+)
+
+//go:linkname gocamLogger github.com/svanichkin/gocam.camLog
+var gocamLogger *log.Logger
+
+func init() {
+	if gocamLogger == nil {
+		return
+	}
+	gocamLogger.SetOutput(gocamLogWriter{})
+	gocamLogger.SetFlags(0)
+	gocamLogger.SetPrefix("")
+}
+
+type gocamLogWriter struct{}
+
+func (gocamLogWriter) Write(p []byte) (int, error) {
+	msg := strings.TrimRight(string(p), "\r\n")
+	if msg == "" {
+		return len(p), nil
+	}
+	logs.LogV("%s", msg)
+	return len(p), nil
+}
